8a686c1ccb01916efdb358196edbeca6da9c048f
Cameron Kurotori <cpkurotori@gmail.com>
2017-02-05 13:18:33 -0800

    added necessary files

---
+++ dbcredentials.py
@@ -0,0 +1,17 @@
+
+def getDatabase():
+	return "Roster"
+
+def getHost():
+	return "dscs-mysql-instance.ca0mrwstseyb.us-west-1.rds.amazonaws.com"
+
+def getUser():
+	return "DSCSAdmin"
+
+def getPass():
+	return "MwCk2016!"
+def getAdminU ():
+	return "dotslashcomputerscience"
+
+def getAdminP():
+	return "dotslash!"
