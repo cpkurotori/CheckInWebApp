2d6fd31251ace13e14ccedeb618dc14b35b99bd3
Cameron Kurotori <cpkurotori@gmail.com>
2017-02-05 00:09:04 -0800

    relocalizing

---
+++ application.py
@@ -0,0 +1,94 @@
+from flask import Flask, request
+from flask.ext.mysql import MySQL
+import checkIn, datetime, .dbcredentials
+
+# Open connection to the database using the login credentials of the given username and password parameters
+
+# EB looks for an 'applicationlication' callable by default.
+application = Flask(__name__)
+
+mysql = MySQL()
+#application.config['MYSQL_DATABASE_DB'] = 'Roster'
+#application.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#application.config['MYSQL_DATABASE_USER'] = 'root'
+#application.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+mysql.init_app(application)
+
+
+@application.route("/")
+def index():
+	return application.send_static_file("checkIn.html")
+
+@application.route("/home", methods=["post"])
+def home():
+	return application.send_static_file("checkIn.html")
+
+@application.route("/admin", methods=["post"])
+def admin():
+
+
+	if db==False:
+		return application.send_static_file("index.html")
+	else:
+		return application.send_static_file("checkIn.html")
+
+@application.route("/checkInPrompt", methods=["post"])
+def checkInPrompt():
+	member = request.form['member']
+	if member == "new":
+		return application.send_static_file("newmember.html")
+	elif member == "returning":
+		return application.send_static_file("returningmember.html")
+	elif member == "update":
+		return application.send_static_file("updateinformation.html")
+
+@application.route("/newMember", methods=["post"])
+def newMember():
+	first = request.form['first']
+	last = request.form['last']
+	uname = request.form['uname']
+	email = request.form['email']
+	phone = request.form['phone']
+	test = checkIn.checkDuplicate (mysql, "username",uname)
+	now = datetime.datetime.now()
+	date = now.strftime("%Y-%m-%d")
+	if test==True:
+		return application.send_static_file("newmember.html")
+	else:
+		checkIn.createNew (mysql, first, last, uname, email, phone, date)
+		checkIn.checkIn (mysql, uname, date)
+	return application.send_static_file("checkIn.html")
+
+@application.route("/returningMember", methods=["post"])
+def returningMember():
+	uname = request.form['uname']
+	test = checkIn.checkDuplicate (mysql,"username",uname)
+	now = datetime.datetime.now()
+	date = now.strftime("%Y-%m-%d")
+	if test==False:
+		return application.send_static_file("returningmember.html")
+	else:
+		checkIn.checkIn (mysql, uname, date)
+	return application.send_static_file("checkIn.html")
+
+@application.route("/updateInformation", methods=["post"])
+def updateInformation():
+	first = request.form['first']
+	last = request.form['last']
+	field = request.form['field']
+	updatedInfo = request.form['updatedInfo']
+	now = datetime.datetime.now()
+	date = now.strftime("%Y-%m-%d")
+	test = checkIn.checkNameDuplicate (mysql,first,last)
+	if test==True:
+		checkIn.updateInfo (mysql,first,last,field,updatedInfo)
+	else:
+		return application.send_static_file("updateinformation.html")
+	return application.send_static_file("checkIn.html")
+
+# run the application.
+if __name__ == "__main__":
+	# Setting debug to True enables debug output. This line should be
+	# removed before deploying a production application.
+	# application.debug = True
+	application.run()
---
+++ checkIn.py
@@ -0,0 +1,58 @@
+from flask.ext.mysql import MySQL
+
+def createNew (mysql, first, last, uname, email, phone, date):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("INSERT INTO MemberInformation (firstname, lastname, username, email, phone_number, date_joined) VALUES (\""+first+"\",\""+last+"\",\""+uname+"\",\""+email+"\",\""+phone+"\",\""+date+"\")")
+	conn.commit()
+	cursor.close()
+	conn.close()
+
+def checkIn (mysql, uname, date):
+	# selects the roster id of the person wtih the username that matches the  parameter uname
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT roster_id FROM MemberInformation WHERE username=\""+str(uname)+"\"")
+	id = cursor.fetchall()[0][0]			# stores the query results
+	cursor.execute("INSERT INTO Attendance (roster_id,date) VALUES("+str(id)+",\""+str(date)+"\")")
+	conn.commit()
+	cursor.close()
+	conn.close()
+
+def checkDuplicate (mysql, field, test):
+	conn = mysql.connect()
+	cursor = conn.cursor()	
+	cursor.execute("SELECT COUNT(*) FROM MemberInformation WHERE "+str(field)+"=\""+str(test)+"\"")
+	count = cursor.fetchall()[0][0]
+	if count != 0:
+		cursor.close()
+		conn.close()	
+		return True		#indicates duplicate is found
+	else:
+		cursor.close()
+		conn.close()		
+		return False	#indicated no duplicate is found
+
+def checkNameDuplicate (mysql, first, last,):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT COUNT(*) FROM MemberInformation WHERE firstname=\""+str(first)+"\" AND lastname=\""+last+"\"")
+	count = cursor.fetchall()[0][0]
+	if count == 1:
+		cursor.close()
+		conn.close()
+		return True		#indicates name found
+	else:
+		cursor.close()
+		conn.close()
+		return False	#indicated name not found
+
+def updateInfo (mysql, first, last, field, updatedInfo):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT roster_id FROM MemberInformation WHERE firstname=\""+str(first)+"\" AND lastname=\""+str(last)+"\"")
+	id = cursor.fetchall()[0][0]			# stores the query results
+	cursor.execute("UPDATE MemberInformation SET "+str(field)+"=\""+str(updatedInfo)+"\" WHERE roster_id="+str(id))
+	conn.commit()
+	cursor.close()
+	conn.close()
